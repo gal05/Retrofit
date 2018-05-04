909e633f7b6aa269525167a757f0d2051d36354e
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-05-04 14:09:19 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/tecsup/guerra/retrofit/avtivities/MainActivity.java
@@ -0,0 +1,15 @@
+package pe.edu.tecsup.guerra.retrofit.avtivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pe.edu.tecsup.guerra.retrofit.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
