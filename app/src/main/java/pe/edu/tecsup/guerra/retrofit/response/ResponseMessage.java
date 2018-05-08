c389b4f544551041a6042fe6b97be3f446638ef1
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-05-08 09:35:43 -0500

    registro productos done!

---
+++ app/src/main/java/pe/edu/tecsup/guerra/retrofit/response/ResponseMessage.java
@@ -0,0 +1,38 @@
+package pe.edu.tecsup.guerra.retrofit.response;
+
+/**
+ * Created by Alumno on 8/05/2018.
+ */
+
+public class ResponseMessage {
+
+
+    private String type;
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseMessage{" +
+                "type='" + type + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+
+}
--- app/src/main/java/pe/edu/tecsup/guerra/retrofit/services/ApiService.java
+++ app/src/main/java/pe/edu/tecsup/guerra/retrofit/services/ApiService.java
@@ -2,9 +2,17 @@ package pe.edu.tecsup.guerra.retrofit.services;
 
 import java.util.List;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import pe.edu.tecsup.guerra.retrofit.models.Producto;
+import pe.edu.tecsup.guerra.retrofit.response.ResponseMessage;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 /**
  * Created by Alumno on 4/05/2018.
@@ -16,5 +24,23 @@ public interface ApiService {
 
     @GET("api/v1/productos")
     Call<List<Producto>> getProductos();
+
+
+
+
+
+    @FormUrlEncoded
+    @POST("/api/v1/productos")
+    Call<ResponseMessage> createProducto(@Field("nombre") String nombre,
+                                         @Field("precio") String precio,
+                                         @Field("detalles") String detalles);
+    @Multipart
+    @POST("/api/v1/productos")
+    Call<ResponseMessage> createProductoWithImage(
+            @Part("nombre") RequestBody nombre,
+            @Part("precio") RequestBody precio,
+            @Part("detalles") RequestBody detalles,
+            @Part MultipartBody.Part imagen
+    );
 
 }
