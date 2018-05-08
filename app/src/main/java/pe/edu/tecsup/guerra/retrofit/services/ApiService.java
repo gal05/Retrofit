743b1b23607fd6cc1c6780a7189dd2e61614728a
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-05-08 08:51:07 -0500

    8/5/18

--- app/src/main/java/pe/edu/tecsup/guerra/retrofit/services/ApiService.java
+++ app/src/main/java/pe/edu/tecsup/guerra/retrofit/services/ApiService.java
@@ -12,7 +12,7 @@ import retrofit2.http.GET;
 
 public interface ApiService {
 
-    String API_BASE_URL = "https://retrofit-android-gal05.c9users.io";
+    String API_BASE_URL = "https://laravel-55-android-gal05.c9users.io";
 
     @GET("api/v1/productos")
     Call<List<Producto>> getProductos();
